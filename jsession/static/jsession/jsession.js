e64c35451bc7fa4dfa9effa225c01b33d428414d
David Novakovic <davidnovakovic@gmail.com>
2013-06-15 22:12:58 +1000

    Set values from js.

--- jsession/static/jsession/jsession.js
+++ jsession/static/jsession/jsession.js
@@ -3,13 +3,15 @@ $(function() {
 	window.JSESSION = JSESSION;
 
     JSESSION.init = {
+        withQuotes: false,
         jsessionCookie : 'jsession',
         parseCookie : function() {
-            var cookies = $.cookie(JSESSION.init.jsessionCookie)
+            var cookies = $.cookie(JSESSION.init.jsessionCookie);
             if(cookies) {
                 var x = '';
                 if(cookies.charAt(0) == '"' && cookies.charAt(cookies.length-1) == '"') {
                     x = $.base64Decode(cookies.substring(1,cookies.length-1));
+                    JSESSION.init.withQuotes = '"';
                 }
                 else {
                     x = $.base64Decode(cookies);
@@ -19,5 +21,19 @@ $(function() {
             }
             return false;
         }
+    }
+    JSESSION.set = function(key, value) {
+            var data = JSESSION.init.parseCookie();
+            data[key] = value;
+            var json = JSON.stringify(data);
+            if(JSESSION.init.withQuotes){
+                json = '"'+json+'"';
+            }
+            var b64 = $.base64Encode(json);
+            $.cookie(JSESSION.init.jsessionCookie, b64);
+    }
+    JSESSION.get = function(key) {
+            var data = JSESSION.init.parseCookie();
+            return data[key];
     }
 });
